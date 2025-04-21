43fc4dceb9f5621a1032976cc6658d65ea5fb950
tdchoi <ctd2580@naver.com>
2025-04-21 17:49:36 +0900

    tdchoi test

--- Front/src/pages/Modal.js
+++ Front/src/pages/Modal.js
@@ -32,7 +32,7 @@ const modal = ({ show, onHide }) => {
                         laboriosam. Quibusdam facilis doloribus debitis! Sit quasi quod
                         accusamus eos quod. Ab quos consequuntur eaque quo rem! Mollitia
                         reiciendis porro quo magni incidunt dolore amet atque facilis ipsum
-                        deleniti rem!                        Ipsum molestiae natus adipisci modi eligendi? Debitis amet quae unde
+                        deleniti rem!
                     </p>
                 </Modal.Body>
                 <Modal.Footer>
