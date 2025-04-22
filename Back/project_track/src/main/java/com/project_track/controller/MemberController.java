5eae5207ed945da34d999e7012a3c15480b5a132
KimJeHyun91 <kjh910920@gmail.com>
2025-04-22 10:30:08 +0900

    04/22 10:26 수정 (#15)
    * 수정1234
    
    * Create 1234.txt
    
    * Delete 1234.txt
    
    * STS4, MySQL문, 등 수정
    
    - STS4 구동시 오류 부분 수정
    - 잘못된 MySQL문 수정
    - 테스트 파일 삭제

--- Back/project_track/src/main/java/com/project_track/controller/MemberController.java
+++ Back/project_track/src/main/java/com/project_track/controller/MemberController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.project_track.service.MemberService;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/member")
 public class MemberController {
 	@Autowired
 	MemberService Member_service;
