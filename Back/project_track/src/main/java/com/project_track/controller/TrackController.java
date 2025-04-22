f5a65e816f5cdc9b63ce97af9a72a62cdbf2eee8
KimJeHyun91 <kjh910920@gmail.com>
2025-04-22 12:48:41 +0900

    컨트롤러 수정 (#16)
    * 수정1234
    
    * Create 1234.txt
    
    * Delete 1234.txt
    
    * STS4, MySQL문, 등 수정
    
    - STS4 구동시 오류 부분 수정
    - 잘못된 MySQL문 수정
    - 테스트 파일 삭제
    
    * 컨트롤러 수정

--- Back/project_track/src/main/java/com/project_track/controller/TrackController.java
+++ Back/project_track/src/main/java/com/project_track/controller/TrackController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project_track.domain.TrackDomain;
-import com.project_track.repository.TrackRepository;
+import com.project_track.service.TrackService;
 
 @RestController
 @RequestMapping("/track")
@@ -20,25 +20,14 @@ import com.project_track.repository.TrackRepository;
 public class TrackController {
 
 	@Autowired
-	private TrackRepository trackRepository;
+	TrackService track_service;
 
-	@GetMapping("/all")
-	public List<Map<String, String>> getAllTracks() {
-		// DB에서 모든 Track 엔티티 조회
-		List<TrackDomain> tracks = trackRepository.findAll();
+	@GetMapping("/get_track_list_all")
+	public List<TrackDomain> get_track_list_all() {
+		
+		List<TrackDomain> track_list=track_service.get_track_list_all();
 
-		// 결과 담을 리스트 선언(각 트랙정보를 Map<String,String> 형태로 담을 것)
-		List<Map<String, String>> result = new ArrayList<>();
-
-		// 각 Track 객체를 하나씩 반복하면서 Map으로 변환
-		for (TrackDomain t : tracks) {
-			Map<String, String> map = new HashMap<>();
-			map.put("name", t.getTrack_name()); // "name" 키에 산 이름 저장
-			map.put("location", t.getTrack_location()); // "Location" 키에 위경도 저장ㄴ
-			result.add(map);
-		}
-
-		return result;
+		return track_list;
 	}
 
 }
