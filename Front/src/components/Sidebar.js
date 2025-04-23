8fbc5ab846b96cec312bee8469926f5a7a77e0a1
tdchoi <ctd2580@naver.com>
2025-04-23 18:23:05 +0900

    slide 수정

--- Front/src/components/Sidebar.js
+++ Front/src/components/Sidebar.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import "./Sidebar.css";
+import { BiSearch } from "react-icons/bi";
+import Search from "./search/Search";
+import SearchList from "./search/SearchList";
 
 export default function SideBar() {
     const [open, setOpen] = useState(false);
@@ -7,14 +10,15 @@ export default function SideBar() {
     const handleToggle = () => {
         setOpen((prev) => !prev);
     };
-
+    
     return (
         <div className={`sidebar-wrapper ${open ? "open" : "closed"}`}>
-            <div className="sidebar-content">
-                <p>당일치기/1박2일 checkbox</p>
-                <p>소요시간</p>
-                <p>트랙 난이도 </p>
-                <p></p>
+            <div className="sidebar-content" style={{maxHeight: "100vh", overflowY: "auto",}}>
+                <Search />
+                <SearchList />
+                <SearchList />
+                <SearchList />
+                <SearchList /><SearchList /><SearchList /><SearchList /><SearchList />
             </div>
             <div className="sidebar-toggle" onClick={handleToggle}>
                 {open ? "←" : "→"}
