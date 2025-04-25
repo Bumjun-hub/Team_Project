fa7b5ac5cfaacb922871a3ea1619cf83313d6687
KimJeHyun91 <kjh910920@gmail.com>
2025-04-25 17:24:59 +0900

    STS 수정

---
+++ Back/project_track/src/main/java/com/project_track/repository/TrackRepository.java
@@ -0,0 +1,12 @@
+package com.project_track.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.project_track.domain.TrackDomain;
+import com.project_track.id.TrackId;
+
+@Repository
+public interface TrackRepository extends JpaRepository<TrackDomain, TrackId>{
+
+}
