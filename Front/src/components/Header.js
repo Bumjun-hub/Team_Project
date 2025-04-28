c812315525a05a1a8b3752809b2a824ace284a41
ANJIYOUNG0109 <jiyoung0109@naver.com>
2025-04-28 10:07:45 +0900

    주말 작업

--- Front/src/components/Header.js
+++ Front/src/components/Header.js
@@ -6,13 +6,14 @@ import Modal from './../pages/Modal';
 import DetailModal from '../pages/DetailModal';
 import { Link, Links, Routes } from 'react-router-dom';
 import Login from '../pages/Login';
-import Member from '../pages/Member';
+import JoinModal from "./join/JoinModal"
 import { FaUserPlus } from "react-icons/fa";
 import { MdLogin } from "react-icons/md";
 import { FaHome } from "react-icons/fa";
 
 const Header = (() => {
     const [showModal, setShowModal] = useState(false);
+    const [showLogin, setShowLogin] = useState(false); // 지영 로그인 모달 추가
     return (
         <>
             <div className='HeadDiv'>
@@ -25,13 +26,17 @@ const Header = (() => {
 
                 <div className='RightDiv'>
                     <div className='Login'>
-                        <Link to="/login" className="LoginButton">
-                            <MdLogin className="MdLogin" />
-                        </Link>
+                      {/* 지영 로그인 모달 */}
+                      <button className="LoginButton" onClick={()=> setShowLogin(true)}>
+                        <MdLogin className="MdLogin" />
+                      </button>
+                        <JoinModal show={showLogin} onHide={()=> setShowLogin(false)}>                       
+                            <Login onClose={()=> setShowLogin(false)} />
+                        </JoinModal>
 
-                        <Link to="/member" className="MemberButton">
-                            <FaUserPlus className="FaUserPlus" />
-                        </Link>
+                      <Link to="/member" className="MemberButton">
+                        <FaUserPlus className="FaUserPlus" />
+                      </Link>
                     </div>
                 </div>
 
