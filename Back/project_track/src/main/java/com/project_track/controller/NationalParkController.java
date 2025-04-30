9b75e0587dd51b8163b7555ab04325c2bc71d87d
KimJeHyun91 <kjh910920@gmail.com>
2025-04-30 12:51:12 +0900

    백엔드 수정 (#36)
    * 4/25 11시
    
    * STS 수정
    
    * 백엔드 대대적인 수정
    
    * db 수정, front 기상 추가
    
    * 백엔드 class 수정
    
    ---------
    
    Co-authored-by: Bumjun-hub <jc971017@gmail.com>

--- Back/project_track/src/main/java/com/project_track/controller/NationalParkController.java
+++ Back/project_track/src/main/java/com/project_track/controller/NationalParkController.java
@@ -4,7 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,7 +34,11 @@ public class NationalParkController {
 	
 	@GetMapping("/get_one_object")
 	public NationalParkDomain get_one_object(@RequestParam("national_park_no") Integer national_park_no) {
+		List<NationalParkDomain> temp_list=national_park_service.get_all_list();
 		if(national_park_no!=null) {
+			if(national_park_no<1||national_park_no>temp_list.size()) {
+				return null;
+			}
 			Optional<NationalParkDomain> result=national_park_service.get_one_object(national_park_no);
 			if(result.isEmpty()) {
 				return null;
@@ -39,6 +48,91 @@ public class NationalParkController {
 		return null;
 	}
 	
+	@PostMapping("/add")
+	public int add(@RequestBody NationalParkDomain national_park_domain) {
+		if(
+				national_park_domain.getNational_park_no()==null||
+				!StringUtils.hasText(national_park_domain.getNational_park_name())||
+				!StringUtils.hasText(national_park_domain.getNational_park_official_website())||
+				!StringUtils.hasText(national_park_domain.getNational_park_introduce())||
+				national_park_domain.getNational_park_latitude()==null||
+				national_park_domain.getNational_park_longitude()==null||
+				!StringUtils.hasText(national_park_domain.getNational_park_address_1())||
+				!StringUtils.hasText(national_park_domain.getNational_park_address_2())||
+				!StringUtils.hasText(national_park_domain.getNational_park_address_3())
+		  ) {
+			return 1900;
+		}
+		List<NationalParkDomain> temp_list=national_park_service.get_all_list();
+		if(national_park_domain.getNational_park_no()>=1&&national_park_domain.getNational_park_no()<=temp_list.size()) {
+			return 1102;
+		}
+		NationalParkDomain result_national_park=national_park_service.add(national_park_domain);
+		if(result_national_park!=null) {
+			return 1100;
+		}
+		return 1101;
+	}
 	
+	@PutMapping("/modify")
+	public int modify(@RequestBody NationalParkDomain national_park_domain) {
+		if(national_park_domain.getNational_park_no()==null) {
+			return 1900;
+		}
+		List<NationalParkDomain> temp_list=national_park_service.get_all_list();
+		if(national_park_domain.getNational_park_no()<1||national_park_domain.getNational_park_no()>temp_list.size()) {
+			return 1112;
+		}
+		Optional<NationalParkDomain> result_optional_national_park=national_park_service.get_one_object(national_park_domain.getNational_park_no());
+		if(result_optional_national_park.isPresent()) {
+			NationalParkDomain result=result_optional_national_park.get();
+			if(!StringUtils.hasText(national_park_domain.getNational_park_name())) {
+				national_park_domain.setNational_park_name(result.getNational_park_name());
+			}
+			if(!StringUtils.hasText(national_park_domain.getNational_park_official_website())) {
+				national_park_domain.setNational_park_official_website(result.getNational_park_official_website());
+			}
+			if(!StringUtils.hasText(national_park_domain.getNational_park_introduce())) {
+				national_park_domain.setNational_park_introduce(result.getNational_park_introduce());
+			}
+			if(national_park_domain.getNational_park_latitude()==null) {
+				national_park_domain.setNational_park_latitude(result.getNational_park_latitude());
+			}
+			if(national_park_domain.getNational_park_longitude()==null) {
+				national_park_domain.setNational_park_longitude(result.getNational_park_longitude());
+			}
+			if(!StringUtils.hasText(national_park_domain.getNational_park_address_1())) {
+				national_park_domain.setNational_park_address_1(result.getNational_park_address_1());
+			}
+			if(!StringUtils.hasText(national_park_domain.getNational_park_address_2())) {
+				national_park_domain.setNational_park_address_2(result.getNational_park_address_2());
+			}
+			if(!StringUtils.hasText(national_park_domain.getNational_park_address_3())) {
+				national_park_domain.setNational_park_address_3(result.getNational_park_address_3());
+			}
+			NationalParkDomain result_national_park=national_park_service.modify(national_park_domain);
+			if(result_national_park!=null) {
+				return 1110;
+			}
+			return 1111;	
+		}else {
+			return 1111;
+		}
+	}
 	
+	@DeleteMapping("/delete")
+	public int delete(@RequestBody NationalParkDomain national_park_domain) {
+		if(national_park_domain.getNational_park_no()==null) {
+			return 1900;
+		}
+		List<NationalParkDomain> temp_list=national_park_service.get_all_list();
+		if(national_park_domain.getNational_park_no()<1||national_park_domain.getNational_park_no()>temp_list.size()) {
+			return 1112;
+		}
+		Optional<NationalParkDomain> result_national_park=national_park_service.delete(national_park_domain.getNational_park_no());
+		if(result_national_park.isEmpty()) {
+			return 1120;
+		}
+		return 1121;
+	}
 }
